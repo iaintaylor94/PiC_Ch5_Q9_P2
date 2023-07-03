4931d6886b5300c74ad89d44fcde1a97ee30b798
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-03 13:04:43 +0000

    replaced for with while

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 /* Program to calculate the 200th triangular number, introduction to the for statement. */
+/* Replace for with while */
 
 #include <stdio.h>
 
@@ -8,8 +9,10 @@ int main(void) {
 
   triangularNumber = 0;
 
-  for (n = 1; n <= 200; n = n + 1) {
+  n = 1;
+  while (n <= 200) {
     triangularNumber = triangularNumber + n;
+    n = n + 1;
   }
 
   printf ("The 200th triangular number is %i\n", triangularNumber);
