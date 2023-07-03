ba662d3514881c879a1a478d0c4a24a24ca630ab
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-03 12:57:33 +0000

    revert to previous commit

--- main.c
+++ main.c
@@ -8,10 +8,8 @@ int main(void) {
 
   triangularNumber = 0;
 
-  n = 1;
-  while (n <= 200) {
+  for (n = 1; n <= 200; n = n + 1) {
     triangularNumber = triangularNumber + n;
-    n = n + 1;
   }
 
   printf ("The 200th triangular number is %i\n", triangularNumber);
